f504d9f1abba36189fced90386526349e3a919cb
Duchuy <huy.hd@rubycell.com>
2020-11-15 09:58:38 +0700

    add run and rabbit sprites

--- Assets/_Game/Scripts/Gameplay/Player.cs
+++ Assets/_Game/Scripts/Gameplay/Player.cs
@@ -6,18 +6,22 @@ using Mirror;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : NetworkBehaviour {
     public static float SPEED = 10f;
-    public static float JUMP_FORCE = 1000f;
+    public static float JUMP_FORCE = 1500f;
     public static float SHOOT_RATE = 0.2f;
 
     public string state = "";
+    public string direction = "left";
     public Rigidbody2D rb2d;
     public Transform barrel;
 
     bool isShooting = false;
+    bool isRunning = false;
     float nextShoot = 0f;
+    int jumpCount;
 
     void Start() {
-        rb2d = GetComponent<Rigidbody2D>();    
+        rb2d = GetComponent<Rigidbody2D>();
+        jumpCount = 2;
     }
 
     public override void OnStartLocalPlayer() {
@@ -29,32 +33,75 @@ public class Player : NetworkBehaviour {
 
     void Update() {
         if (Joystick.Instance.Horizontal > 0 || Input.GetKeyDown(KeyCode.RightArrow)) {
-            GetComponent<SpriteRenderer>().flipX = false;
-            rb2d.velocity = new Vector3(SPEED, rb2d.velocity.y);
-        }
-        else if (Joystick.Instance.Horizontal < 0 || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            GetComponent<SpriteRenderer>().flipX = true;
-            rb2d.velocity = new Vector3(-SPEED, rb2d.velocity.y);
-        }
-        else
+            direction = "right";
+
+            if (isRunning) {
+                rb2d.velocity = new Vector3(SPEED * 1.5f, rb2d.velocity.y);
+            } else {
+                rb2d.velocity = new Vector3(SPEED, rb2d.velocity.y);
+            }
+
+            if (state != "jump" && state != "fall") {
+                if (isRunning)
+                    state = "run";
+                else
+                    state = "go";
+            }
+
+        } else if (Joystick.Instance.Horizontal < 0 || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            direction = "left";
+
+            if (isRunning) {
+                rb2d.velocity = new Vector3(-SPEED * 1.5f, rb2d.velocity.y);
+            } else {
+                rb2d.velocity = new Vector3(-SPEED, rb2d.velocity.y);
+            }
+
+            if (state != "jump" && state != "fall") {
+                if (isRunning)
+                    state = "run";
+                else
+                    state = "go";
+            }
+        } else {
             rb2d.velocity = new Vector3(0, rb2d.velocity.y);
+            if (state != "jump" && state != "fall")
+                state = "stand";
+        }
 
         if (Input.GetKeyDown(KeyCode.Space)) {
             Jump();
         }
 
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Z)) {
+            isRunning = false;
+        }
+
         if (isShooting && Time.time > nextShoot) {
             nextShoot = Time.time + SHOOT_RATE;
-
-            //GameObject bullet = Instantiate(Resources.Load<GameObject>("bullet"), transform.position, Quaternion.identity);
             GameObject bullet = Gameplay.LoadPool("bullet", barrel.position);
-            Debug.Log("SHOOT!");
         }
     }
 
     public void Jump() {
+        if (jumpCount <= 0)
+            return;
+
+        jumpCount--;
         rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
         rb2d.AddForce(new Vector2(0, JUMP_FORCE));
+        state = "jump";
+        StartCoroutine(Fall(jumpCount));
+    }
+
+    public IEnumerator Fall(int jumpId) {
+        yield return new WaitForSeconds(0.35f);
+        if (jumpCount == jumpId) {
+            state = "fall";
+        }
     }
 
     public void ShootStart() {
@@ -65,7 +112,32 @@ public class Player : NetworkBehaviour {
         isShooting = false;
     }
 
+    public void RunStart() {
+        isRunning = true;
+    }
+
+    public void RunEnd() {
+        isRunning = false;
+    }
+
     public void UseSkill() {
         Debug.Log("USE SKILL!");
     }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("THIS IS TRIGGER ENTER 2D");
+        if (collision.gameObject.CompareTag("Ground")) {
+            if (Mathf.Abs(Joystick.Instance.Horizontal) > 0 || Input.GetKeyDown(KeyCode.RightArrow)) {
+                if (isRunning)
+                    state = "run";
+                else
+                    state = "go";
+            } else {
+                state = "stand";
+            }
+            jumpCount = 2;
+        }
+    }
+
+
 }
---
+++ Assets/_Game/Scripts/Gameplay/PlayerAnimationUpdate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(FramesAnimator))]
+public class PlayerAnimationUpdate : MonoBehaviour {
+    public Sprite[] go;
+    public Sprite[] stand;
+    public Sprite[] jump;
+    public Sprite[] fall;
+    public Sprite[] run;
+
+    Player player;
+    FramesAnimator animator;
+
+    void Start() {
+        player = GetComponent<Player>();
+        animator = GetComponent<FramesAnimator>();
+    }
+
+    void Update() {
+        if (player.state == "stand") {
+            animator.spritesheet = stand;
+        } else if (player.state == "go") {
+            animator.spritesheet = go;
+        } else if (player.state == "jump") {
+            animator.spritesheet = jump;
+        } else if (player.state == "fall") {
+            animator.spritesheet = fall;
+        } else if (player.state == "run") {
+            animator.spritesheet = run;
+        }
+
+        if (player.direction == "left") {
+            GetComponent<SpriteRenderer>().flipX = false;
+        } else {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+    }
+}
