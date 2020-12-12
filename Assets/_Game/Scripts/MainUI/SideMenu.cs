4be2ca85dd4750aec1711cf71a8c6fe15065876b
Duchuy <huy.hd@rubycell.com>
2020-12-13 03:05:59 +0700

    add aventure scene

---
+++ Assets/_Game/Scripts/MainUI/SideMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SideMenu : MonoBehaviour
+{
+    //public static SideMenu Instance;
+
+    //private void Awake() {
+    //    if (!Instance) {
+    //        Instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    } else {
+    //        Destroy(gameObject);
+    //    }
+    //}
+
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
